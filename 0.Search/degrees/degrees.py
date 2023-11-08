abc09379ae1ca7df6ba309a18bf86842142aad92
Emir Balcı <200101018@ogr.atu.edu.tr>
2023-11-08 10:38:09 +0300

    Update degrees.py

--- 0.Search/degrees/degrees.py
+++ 0.Search/degrees/degrees.py
@@ -6,7 +6,7 @@ from util import Node, StackFrontier, QueueFrontier
 # Maps names to a set of corresponding person_ids
 names = {}
 
-# Maps person_ids to a dictionary of: name, birth, movies (a set of movie_ids)
+# Maps person_ids to a dictionary of: name, birth, movies (a set of movie_ids) deneme
 people = {}
 
 # Maps movie_ids to a dictionary of: title, year, stars (a set of person_ids)
